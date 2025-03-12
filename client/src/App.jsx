c4a96d402c569930b82b6ad60c4256280bd61b9b
harishankathi8 <varmascode1243@gmail.com>
2025-03-12 11:25:04 +0530

    created the pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import Projects from "./pages/Projects.jsx"
+import Signin from "./pages/Signin.jsx"
+import SignUp from "./pages/SignUp.jsx"
+import Dashboard from "./pages/Dashboard.jsx"
+
 const App = ()=>{
   return (
-    <>
-      <h1>Hello welcome to Blog</h1>
-    </>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/"  element={<Home/>}/>
+          <Route path="/about" element={<About/>} />
+          <Route path="/signin" element={<Signin/>}/>
+          <Route path="/signup" element={<SignUp/>}/> 
+          <Route path="/dashboard" element={<Dashboard/>}/>
+           <Route path="/Projects" element={<Projects/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
 export default App
