8d34fcbde3ca09b8bf0539929a60cf8cfad53bd9
harishankathi8 <varmascode1243@gmail.com>
2025-03-13 15:27:02 +0530

    created the footer components response

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Home from "./pages/Home.jsx"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Header from "./components/Header.jsx"
 import About from "./pages/About.jsx"
+import Dashboard from "./pages/Dashboard.jsx"
+import Home from "./pages/Home.jsx"
 import Projects from "./pages/Projects.jsx"
 import Signin from "./pages/Signin.jsx"
 import SignUp from "./pages/SignUp.jsx"
-import Dashboard from "./pages/Dashboard.jsx"
-import Header from "./components/Header.jsx"
+import Footercom from "./components/Footer.jsx"
 
-const App = ()=>{
+const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
-        <Routes>
-          <Route path="/"  element={<Home/>}/>
-          <Route path="/about" element={<About/>} />
-          <Route path="/signin" element={<Signin/>}/>
-          <Route path="/signup" element={<SignUp/>}/> 
-          <Route path="/dashboard" element={<Dashboard/>}/>
-           <Route path="/Projects" element={<Projects/>}/>
-        </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
+      <Footercom />
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,75 @@
+import { Footer, FooterCopyright, FooterIcon } from 'flowbite-react'
+import React from 'react'
+import {BsFacebook, BsGithub, BsInstagram, BsTwitter, BsWhatsapp} from "react-icons/bs"
+import { Link } from 'react-router-dom'
+
+const Footercom = () => {
+  return (
+        <Footer container className='border border-t-8 border-teal-500'>
+          <div className="w-full max-w-7xl mx-auto">
+            <div className=" grid w-full justify-between sm:flex md:grid-cols-1">
+                <div className="mt-5">
+                <Link className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Varma's</span>Blog
+        </Link>
+                </div>
+                <div className='grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6'>
+                    <div>
+                        <Footer.Title title=' About' />
+                          <Footer.LinkGroup col>
+                            <Footer.Link href='/about'>
+                            About my Blog
+                            </Footer.Link>
+                            <Footer.Link href='/contact'>
+                                contact me
+                            </Footer.Link>
+                            
+                        </Footer.LinkGroup> 
+                        
+                    </div>
+                    <div>
+                        <Footer.Title title='connect on social media' />
+                          <Footer.LinkGroup col>
+                            <Footer.Link href='/about' target='_blank'>
+                                what'sApp
+                            </Footer.Link>
+                            <Footer.Link href='/contact' target='_blank'>
+                                instagram
+                            </Footer.Link>
+                            
+                        </Footer.LinkGroup> 
+                        
+                    </div>
+                    <div>
+                        <Footer.Title title='Legal' />
+                          <Footer.LinkGroup col>
+                            <Footer.Link href='#'>
+                                Privacy Policy
+                            </Footer.Link>
+                            <Footer.Link href="#">
+                                Terms&conditions
+                            </Footer.Link>
+                            
+                        </Footer.LinkGroup> 
+                        
+                    </div>
+                </div>
+            </div>
+            <Footer.Divider />
+            <div className='w-full sm:flex sm:items-center sm:justify-between'>
+                <FooterCopyright by='Harish Varma Ankathi' year={new Date().getFullYear()}/>
+                <div className='flex gap-6 sm:mt-5 mt-4 sm:justify-center  '>
+                    <Footer.Icon href='#' icon={BsFacebook}/>
+                    <Footer.Icon href='#' icon={BsInstagram}/>
+                    <Footer.Icon href='#' icon={BsWhatsapp}/>
+                    <Footer.Icon href='#' icon={BsTwitter}/>
+                    <Footer.Icon href='#' icon={BsGithub}/>
+                   
+                </div>
+            </div>
+          </div>
+        </Footer>
+  )
+}
+
+export default Footercom
