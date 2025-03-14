b6d4ba5e023a20fa165fe3439e9b2a05b67b026c
harishankathi8 <varmascode1243@gmail.com>
2025-03-14 10:17:01 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,9 @@
 import User from "../models/Usermodel.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js"
-const authController = async(req,res,next)=>{
+import jwt from "jsonwebtoken"
+export const signup = async(req,res,next)=>{
         const {username,email,password} = req.body
-
-
         if(!username || !email || !password || username==="" || email==="" || password === ""){
            next(errorHandler(400,"All the fields are required"))
         }
@@ -15,8 +14,7 @@ const authController = async(req,res,next)=>{
             email,
             password:hashedPassword
         })
-        try {
-            
+        try { 
         await Newuser.save()
         res.json({
             success:true,
@@ -29,6 +27,31 @@ const authController = async(req,res,next)=>{
                 })
         }
 
+
 }
 
-export default authController
+export const signin = async(req,res,next)=>{
+            const {email,password} = req.body
+            if(!email || !password || email==="" || password === ""){
+             return next(errorHandler(400,"All the fields are required"))
+            }
+           try {
+                const user = await User.findOne({email})
+                if(!user){
+                    return next(errorHandler(401,"Invalid email or password"))
+                }
+                const comparePasword = bcryptjs.compareSync(password,user.password)
+                if(!comparePasword){
+                    return next(errorHandler(401,"Invalid email or password"))
+                    }
+
+                    // genarate a token
+
+                    const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
+                    const {password:pass, ...rest} = user._doc
+                    res.status(200).cookie("access_token",token,{http:true}).json(rest)
+                
+           } catch (error) {
+                    next(error)
+           }
+}
